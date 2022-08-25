b07c15c4e08dfe6fd884f19f91cc2889360f2fd1
Gustavo Santana <gustavossw@hotmail.com>
2022-08-25 17:07:53 -0300

    build: router-dom, sass, axios

---
+++ src/Router.jsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Subscribe />} />
+      <Route path="/event" element={<Event />} />
+      <Route path="/event/lesson/:slug" element={<Event />} />
+    </Routes>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import "/src/styles/app.module.sass";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
