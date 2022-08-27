3b4135080b93808f70ca61812e8310427da647b9
Gustavo Santana <gustavossw@hotmail.com>
2022-08-27 18:42:29 -0300

    style: tooltip on icons nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,18 +2,33 @@ import { Article, FilmSlate, FilmStrip } from "phosphor-react";
 import { Link } from "react-router-dom";
 
 import styles from "./navbar.module.sass";
+import ReactTooltip from "react-tooltip";
 
 export const Navbar = () => {
   return (
     <nav>
       <ul>
-        <Link to="/">
+        <Link to="/" data-tip="Página inicial" data-text-color="#0fc">
+          <ReactTooltip borderColor="#0fc" border />
           <Article size={32} />
         </Link>
-        <Link to="/category-movies">
+        <Link
+          to="/category-movies"
+          data-tip="Categoria de filmes"
+          data-text-color="#0fc"
+        >
+          <ReactTooltip />
+
           <FilmSlate size={32} />
         </Link>
-        <Link to="/category-series">
+        <ReactTooltip />
+        <Link
+          to="/category-series"
+          data-tip="Categoria de séries"
+          data-text-color="#0fc"
+        >
+          <ReactTooltip />
+
           <FilmStrip size={32} />
         </Link>
       </ul>
