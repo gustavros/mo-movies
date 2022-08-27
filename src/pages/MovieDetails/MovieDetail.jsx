0379e0018818d2f35bae9f62750026abdad80726
Gustavo Santana <gustavossw@hotmail.com>
2022-08-26 21:42:46 -0300

    chore: similars movies

--- src/pages/MovieDetails/MovieDetail.jsx
+++ src/pages/MovieDetails/MovieDetail.jsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Header } from "../../components/Header/Header";
-import { Star } from "phosphor-react";
-import axios from "axios";
 
+import axios from "axios";
 import styles from "./moviedetail.module.sass";
 
 export const MovieDetail = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState([]);
   const [genre, setGenre] = useState([]);
-  const [companies, setCompanies] = useState([]);
+  const [similarMovies, setSimilarMovies] = useState([]);
 
   const URL_IMAGE = "https://image.tmdb.org/t/p/w500";
 
@@ -24,9 +23,21 @@ export const MovieDetail = () => {
       .then((response) => {
         setMovie(response.data);
         setGenre(response.data.genres);
-        setCompanies(response.data.production_companies);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-        console.log(response.data.production_companies);
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${
+          import.meta.env.VITE_KEY
+        }&language=pt-BR&page=1`
+      )
+      .then((response) => {
+        setSimilarMovies(response.data.results);
       })
       .catch((error) => {
         console.error(error);
@@ -37,7 +48,7 @@ export const MovieDetail = () => {
     <>
       <Header />
       <div className={styles.container__movie}>
-        <img src={`${URL_IMAGE}${movie.poster_path}`} alt="" />
+        <img src={`${URL_IMAGE}${movie.poster_path}`} alt={movie.title} />
         <div className={styles.container__movie__content}>
           {genre.map((data) => {
             return (
@@ -52,6 +63,19 @@ export const MovieDetail = () => {
             <span>US$ {movie.budget}</span>
           </div>
         </div>
+      </div>
+
+      <h1>Similares</h1>
+      <div className={styles.contaioner__movie__similar}>
+        {similarMovies &&
+          similarMovies.map((movie) => {
+            return (
+              <div key={movie.id}>
+                <h1>{movie.title}</h1>
+                <img src={`${URL_IMAGE}${movie.backdrop_path}`} alt="" />
+              </div>
+            );
+          })}
       </div>
     </>
   );
